0da9d6a48667d41897888b468d199399c5c35517
Parham Saravani <parhamsaravani83@gmail.com>
2026-08-17 17:55:30 +0330

    feat: add trending movies and series controller

--- backEnd/src/controllers/discover.controller.js
+++ backEnd/src/controllers/discover.controller.js
@@ -1,14 +1,31 @@
 import Serie from "../models/series.model.js";
 import Movie from "../models/movie.model.js";
 const takeSimilarContent = async (req, res) => {
-  const dataSlug = req.params.slug;  
-  const [movie , series] = await Promise.all([Serie.findOne({slug:dataSlug}) , Movie.findOne({slug: dataSlug})]);
-  if(!movie && !series){
-    res.json({message:'NOT_FOUND'})
+  const dataSlug = req.params.slug;
+  const [movie, series] = await Promise.all([
+    Serie.findOne({ slug: dataSlug }),
+    Movie.findOne({ slug: dataSlug }),
+  ]);
+  if (!movie && !series) {
+    res.json({ message: "NOT_FOUND" });
   }
   const current = movie || series;
-  const [similaMovies , similarSeries] = await Promise.all([Serie.find({ _id : {$ne : current._id} , genres: {$in:current.genres} }).limit(5) , Movie.find({ _id : {$ne : current._id } , genres: {$in:current.genres} }).limit(5)])
-  res.json([...similaMovies , ...similarSeries])
+  const [similaMovies, similarSeries] = await Promise.all([
+    Serie.find({
+      _id: { $ne: current._id },
+      genres: { $in: current.genres },
+    }).limit(5),
+    Movie.find({
+      _id: { $ne: current._id },
+      genres: { $in: current.genres },
+    }).limit(5),
+  ]);
+  res.json([...similaMovies, ...similarSeries]);
 };
 
-export { takeSimilarContent };
+const takeTrendContent = async (req, res) => {
+  const totalTrendSeries = await Serie.find({ trending: true });
+  const totalTrendMovies = await Movie.find({ trending: true });
+  res.json([...totalTrendMovies, ...totalTrendSeries]);
+};
+export { takeSimilarContent, takeTrendContent };
--- backEnd/src/routers/discover.route.js
+++ backEnd/src/routers/discover.route.js
@@ -1,6 +1,7 @@
 import Router from "express";
-import { takeSimilarContent } from "../controllers/discover.controller.js";
+import { takeSimilarContent , takeTrendContent} from "../controllers/discover.controller.js";
 const router = Router();
 
+router.get('/trending', takeTrendContent)
 router.get('/similar/:slug', takeSimilarContent)
 export default router;
